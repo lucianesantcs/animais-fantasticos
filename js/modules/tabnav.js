d4028c37ec128552cdbf7586cb017b21ae83029f
Luciane Santos <lucianesantcs@gmail.com>
2020-07-04 16:58:44 -0300

    tabnav refatorado

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,23 +1,31 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  const activeClass = 'ativo';
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove(activeClass);
-    });
-    const animaDirecao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add(activeClass, animaDirecao);
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add(activeClass);
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
+    });
+    const animaDirecao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, animaDirecao);
+  }
 
-    tabMenu.forEach((itemMenu, index) => {
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener('click', () => {
-        activeTab(index);
+        this.activeTab(index);
       });
     });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
+    return this;
   }
 }
