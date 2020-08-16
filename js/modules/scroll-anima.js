a7a8d59a8a3d10503db690cbbcc6f5d5f4b1ea89
Luciane Santos <lucianesantcs@gmail.com>
2020-08-16 12:41:34 -0300

    ajuste no scroll anima

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -3,7 +3,7 @@ import debounce from './debounce.js';
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.5;
+    this.windowMetade = window.innerHeight * 0.6;
 
     this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
