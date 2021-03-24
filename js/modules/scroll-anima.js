2c7d0cf92a4a04f334b7b5f9c80110abf761b7cf
Luciane Santos <lucianesantcs@gmail.com>
2021-03-24 14:19:06 -0300

    aumentado distancia das sessoes em scroll anima

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -3,7 +3,7 @@ import debounce from './debounce.js';
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.6;
+    this.windowMetade = window.innerHeight * 0.8;
 
     this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
